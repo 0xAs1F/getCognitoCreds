e1c30f15e639f756e01e34c06f0867a91618923c
Asif Khan <77784855+0xAs1F@users.noreply.github.com>
2022-03-16 13:22:48 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -9,33 +9,25 @@ parser.add_argument("-a", "--userpoolid",dest ="user_pool_id", help="Enter the U
 parser.add_argument("-r", "--region",dest ="region", help="Enter the region")
 args = parser.parse_args()
 
-
-
 region = args.region
 client = boto3.client('cognito-identity' , region_name=region)
 
 token=args.token
-
 user_pool_id = args.user_pool_id
-
 arn_user_pool = f'cognito-idp.{region}.amazonaws.com/{user_pool_id}'
-
 IdentityPoolId = args.IdentityPoolId
 
 response=client.get_id( IdentityPoolId= IdentityPoolId, Logins ={
        arn_user_pool:token
     })
 
-identity_id = response["IdentityId"]
-
-credentials = client.get_credentials_for_identity(IdentityId=identity_id, Logins ={
+credentials = client.get_credentials_for_identity(IdentityId=response["IdentityId"], Logins ={
        arn_user_pool:token
     })
 
 access_key = credentials['Credentials']['AccessKeyId']
 secret_key = credentials['Credentials']['SecretKey']
 session_token = credentials['Credentials']['SessionToken']
-
 
 print("Access Key: " + access_key)
 print("Secret Key: " + secret_key)
