5438d1278356027bba4eee4b30a75c6bbf25c763
Falcnix <83296245+falcnix@users.noreply.github.com>
2022-03-16 13:58:53 +0530

    updated auth.py

--- auth.py
+++ auth.py
@@ -1,38 +1,33 @@
-import boto3
-import argparse
+import boto3,argparse,sys
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-t", "--idtoken", dest = "token", help="Enter Identity Token")
-parser.add_argument("-p", "--identitypoolid", dest = "IdentityPoolId" , help="Enter Identity Pool ID")
-parser.add_argument("-a", "--userpoolarn",dest ="arn_user_pool", help="Enter the User Pool ARN e.g cognito-idp.us-east-1.amazonaws.com/us-east-1_testuserskldnl")
+parser.add_argument("-i", "--identitypoolid", dest = "IdentityPoolId" , help="Enter Identity Pool ID")
+parser.add_argument("-u", "--userpoolid",dest ="user_pool_id", help="Enter the User Pool ID e.g us-east-1_testuserskldnl")
 parser.add_argument("-r", "--region",dest ="region", help="Enter the region")
 args = parser.parse_args()
 
-region = args.region
-client = boto3.client('cognito-identity' , region_name=region)
+if len(sys.argv) == 1:
+   print('\033[1;31mKindly pass on the necessary arguments or -h/--help for help ')
+   print('Example: python3 auth.py -t eyJraWQiOiJ... -i us-east-1:544545fff6adc-b1ef-4193-aa09-8e9c555ff5b1cc8 -u us-east-2_trrrfrdiHHI -r us-east-1 \033[0;0m')
+   sys.exit()
+else:
+    print("\033[1;36m\nPlease wait while we serve you aws credentials")
+    print("---------------------------------------------- \033[0;0m\n")
 
-token=args.token
+client = boto3.client('cognito-identity' , region_name=args.region)
 
-arn_user_pool =args.arn_user_pool
+user_pool_id = args.user_pool_id
+arn_user_pool = f'cognito-idp.{args.region}.amazonaws.com/{user_pool_id}'
 
-IdentityPoolId = args.IdentityPoolId
-
-response=client.get_id( IdentityPoolId= IdentityPoolId, Logins ={
-       arn_user_pool:token
+response=client.get_id( IdentityPoolId= args.IdentityPoolId, Logins ={
+       arn_user_pool:args.token
     })
 
-identity_id = response["IdentityId"]
-
-credentials = client.get_credentials_for_identity(IdentityId=identity_id, Logins ={
-       arn_user_pool:token
+credentials = client.get_credentials_for_identity(IdentityId=response["IdentityId"], Logins ={
+       arn_user_pool:args.token
     })
 
-access_key = credentials['Credentials']['AccessKeyId']
-secret_key = credentials['Credentials']['SecretKey']
-session_token = credentials['Credentials']['SessionToken']
-
-
-print("Access Key: " + access_key)
-print("Secret Key: " + secret_key)
-print("Session Token: " + session_token)
+print("Access Key: " + "\033[1;32m"+credentials['Credentials']['AccessKeyId'] + "\033[0;0m")
+print("Secret Key: " +"\033[1;32m"+ credentials['Credentials']['SecretKey']+"\033[0;0m")
+print("Session Token: " +"\033[1;32m"+ credentials['Credentials']['SessionToken']+"\033[0;0m")
