4fa9bbe5b1bf88a5e29e9d68492128be35e5288e
asif <asif@mail.com>
2022-03-16 12:57:10 +0530

    user pool arn problem fixed

--- auth.py
+++ auth.py
@@ -5,16 +5,20 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-t", "--idtoken", dest = "token", help="Enter Identity Token")
 parser.add_argument("-p", "--identitypoolid", dest = "IdentityPoolId" , help="Enter Identity Pool ID")
-parser.add_argument("-a", "--userpoolarn",dest ="arn_user_pool", help="Enter the User Pool ARN e.g cognito-idp.us-east-1.amazonaws.com/us-east-1_testuserskldnl")
+parser.add_argument("-a", "--userpoolid",dest ="user_pool_id", help="Enter the User Pool ID e.g us-east-1_testuserskldnl")
 parser.add_argument("-r", "--region",dest ="region", help="Enter the region")
 args = parser.parse_args()
+
+
 
 region = args.region
 client = boto3.client('cognito-identity' , region_name=region)
 
 token=args.token
 
-arn_user_pool =args.arn_user_pool
+user_pool_id = args.user_pool_id
+
+arn_user_pool = f'cognito-idp.{region}.amazonaws.com/{user_pool_id}'
 
 IdentityPoolId = args.IdentityPoolId
 
